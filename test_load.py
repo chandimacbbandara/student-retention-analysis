de660f9de2d6f1bfead457119ee022884c4b440f
Chandima Bandara <it24100307@my.sliit.lk>
2026-09-23 15:12:27 +0530

    done statical model traning and model improvement

--- test_load.py
+++
@@ -1,8 +0,0 @@
-import sklearn.compose._column_transformer
-class _RemainderColsList(list):
-    pass
-sklearn.compose._column_transformer._RemainderColsList = _RemainderColsList
-import joblib
-artifacts = joblib.load('models/final_xgboost_dropout_model.joblib')
-print("Features:", artifacts['feature_names_in'])
-print("Keys:", artifacts.keys())
